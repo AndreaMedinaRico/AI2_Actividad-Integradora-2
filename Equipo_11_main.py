1f27f9e5179130eadd2acb6da3468b32cb56f4f5
AndreaMedinaRico <a01705541@tec.mx>
2024-11-09 12:51:48 -0600

    feat(): ejericicio 2 con actualización de casos prueba

--- Equipo_11_main.py
+++ Equipo_11_main.py
@@ -1,3 +1,6 @@
+"""
+    Problema 2.
+"""
 def nearest_neighbor(graph, start, n):
     """
     Encuentra una ruta aproximada de menor costo usando el algoritmo de vecino más cercano.
@@ -53,6 +56,86 @@ def repeated_nearest_neighbor(graph, n):
 
     return best_path, best_cost
 
+
+"""
+    Problema 3.
+"""
+def bfs(graph, s, t, parent, n):
+    """
+    Función: bfs
+    Parámetros: graph, s, t, parent, n
+            graph: Matriz de flujos
+            s: Nodo de inicio
+            t: Nodo de destino
+            parent: Lista de nodos padres
+            n: Número de nodos
+    Regresa: True si nodo de destino es alcanzable desde el nodo de inicio,
+            False en caso contrario
+    Descripción: Realiza un recorrido en anchura del grafo para encontrar un camino
+                desde el nodo de inicio hasta el nodo de destino.
+    """
+    visited = [False] * n
+    queue = []
+    queue.append(s)
+    visited[s] = True
+
+    while queue:
+        u = queue.pop(0)
+
+        for i in range(len(graph[u])):
+            val =  graph[u][i]
+            if visited[i] == False and val > 0:
+                queue.append(i)
+                visited[i] = True
+                parent[i] = u
+
+    if visited[t]:
+        return True
+    else:
+        return False
+
+
+def ford_fulkerson(graph, inicio, destino, n):
+    """
+    Función: ford_fulkerson
+    Parámetros: graph, inicio, destino, n
+            graph: Matriz de flujos
+            inicio: Nodo de inicio
+            destino: Nodo de destino
+            n: Número de nodos
+    """
+    # Lista de nodos padres
+    parent = [-1] * n
+    # Flujo máximo
+    max_flow = 0
+
+    # Mientras haya caminos existentes de nodo inicio a destino
+    while bfs(graph, inicio, destino, parent, n):
+        # Flujo del camino
+        path_flow = float("Inf")
+        actual = destino
+
+        # Recorre desde nodo destino hasta nodo inicio
+        while actual != inicio:
+            # Mínimo entre flujo del camino y flujo de trayecto actual
+            path_flow = min(path_flow, graph[parent[actual]][actual])
+            actual = parent[actual]
+
+        v = destino
+        # Actualizar flujo de los nodos del camino
+        while v != inicio:
+            u = parent[v]
+            graph[u][v] -= path_flow
+            graph[v][u] += path_flow
+            v = parent[v]
+
+        # Acumular flujo máximo de todos los caminos
+        max_flow += path_flow
+
+    return max_flow
+
+    
+
 def leer_archivo(filename):
     """
     Lee un archivo de texto y convierte su contenido en una matriz de adyacencia para un grafo.
@@ -69,13 +152,22 @@ def leer_archivo(filename):
     
     n = int(lines[0].strip())
     graph = []
-    for line in lines[1:]:
-        graph.append(list(map(int, line.strip().split())))
+    flujos = []
+
+    for i in range(1, n + 1):
+        graph.append(list(map(int, lines[i].strip().split())))
     
-    return n, graph
+    for i in range(n + 1, 2 * n + 1):
+        flujos.append(list(map(int, lines[i].strip().split())))
+    
+    return n, graph, flujos
 
 filename = input("Nombre del archivo: ")
-n, graph = leer_archivo(filename)
+n, graph, flujos = leer_archivo(filename)
+
 path, cost = repeated_nearest_neighbor(graph, n)
+max_flow = ford_fulkerson(flujos, 0, n - 1, n)
+
 print("Mejor ruta encontrada:", path)
 print("Costo de la mejor ruta:", cost)
+print("Flujo máximo:", max_flow)
